43e656850eabedff773c30540879893c57342000
TristanRoss <tgr1998@live.com>
2018-10-02 17:23:00 -0700

    Input/Output Loop Added

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
 public class Calculator {
   
-  public static void main(String[] args) {
-    
-  }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Welcome to the JavaCalculator developed by Tristan Ross");
+        String repeat = "y";
+        while(repeat.equals("y")) {
+            
+            
+            System.out.print("Would you like to perform another calculation? (y / n): ");
+            repeat = input.next();
+        }
+        System.out.println("End program");
+        input.close();
+    }
   
 }
