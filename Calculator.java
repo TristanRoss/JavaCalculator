576fa3c36cc2b19acc999bbd32e6f9b16e2878f3
TristanRoss <tgr1998@live.com>
2018-10-03 19:41:35 -0700

    Subtraction & Decimal Support

--- Calculator.java
+++ Calculator.java
@@ -1,26 +1,32 @@
 import java.util.Scanner;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 public class Calculator {
-  
-
-
     
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Welcome to the JavaCalculator developed by Tristan Ross");
         String repeat = "y";
         while(repeat.equals("y")) {
+            System.out.println();
             options();
             int keycode = input.nextInt();
             switch (keycode) {
                 case 1: System.out.print("How many numbers would you like to add? ");
                         int count = input.nextInt();
-                        double sum = 0;
+                        BigDecimal sum = new BigDecimal(0);
                         System.out.print("Please enter the numbers separated by whitespace: ");
                         for(int i = 0; i < count; i++) {
-                            sum = sum + input.nextDouble();
+                            sum = sum.add(new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP));
                         }
                         printSum(sum, count);
-
+                        break;
+                case 2: System.out.print("Enter the number you would like to subtract from (subtrahend): ");
+                        BigDecimal subtrahend = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        System.out.print("Enter the number you would like to subtract with (minuend): ");
+                        BigDecimal minuend = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        printDifference(subtrahend, minuend);
+                        break;
             }
             
             System.out.print("Would you like to perform another calculation? (y / n): ");
@@ -32,15 +38,17 @@ public class Calculator {
     
     public static void options() {
         System.out.println("Addition (1)");
+        System.out.println("Subtraction (2)");
         System.out.print("Enter key to perform a calculation: ");
     }
     
-    public static void printSum(double sum, int count) {
-        if(sum % 1 == 0) {
-            int total = (int)sum;
-            System.out.println("The sum of the " + count + " numbers is: " + total);
-        } else {
-            System.out.println("The sum of the " + count + " numbers is: " + sum);
-        }
+    public static void printSum(BigDecimal sum, int count) {
+        System.out.println("The sum of the " + count + " numbers is: " + sum.stripTrailingZeros().toPlainString());
+
+    }
+    
+    public static void printDifference(BigDecimal subtrahend, BigDecimal minuend) {   
+        BigDecimal difference = subtrahend.subtract(minuend);
+        System.out.println(subtrahend.stripTrailingZeros().toPlainString() + " - " + minuend.stripTrailingZeros().toPlainString() + " = " + difference.stripTrailingZeros().toPlainString());
     }
 }
