2ddf7278e26cc73874fc9ad38e86d9de6018dca7
TristanRoss <tgr1998@live.com>
2018-10-04 14:42:49 -0700

    Multiplication & Division

--- Calculator.java
+++ Calculator.java
@@ -21,12 +21,26 @@ public class Calculator {
                         }
                         printSum(sum, count);
                         break;
-                case 2: System.out.print("Enter the number you would like to subtract from (subtrahend): ");
+                        
+                case 2: System.out.print("Please enter the number you would like to subtract from (subtrahend): ");
                         BigDecimal subtrahend = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
-                        System.out.print("Enter the number you would like to subtract with (minuend): ");
+                        System.out.print("Please enter the number you would like to subtract with (minuend): ");
                         BigDecimal minuend = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
                         printDifference(subtrahend, minuend);
                         break;
+                        
+                case 3: System.out.print("Please enter the first factor: ");
+                        BigDecimal factorOne = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        System.out.print("Please enter the second factor: ");
+                        BigDecimal factorTwo = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        printProduct(factorOne, factorTwo);
+                        break;
+                        
+                case 4: System.out.print("Please enter the dividend: ");
+                        BigDecimal dividend = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        System.out.print("Please enter the divisor: ");
+                        BigDecimal divisor = new BigDecimal(input.nextDouble()).setScale(8, RoundingMode.HALF_UP);
+                        printQuotient(dividend, divisor);
             }
             
             System.out.print("Would you like to perform another calculation? (y / n): ");
@@ -39,6 +53,8 @@ public class Calculator {
     public static void options() {
         System.out.println("Addition (1)");
         System.out.println("Subtraction (2)");
+        System.out.println("Multiplication (3)");
+        System.out.println("Division (4)");
         System.out.print("Enter key to perform a calculation: ");
     }
     
@@ -50,5 +66,15 @@ public class Calculator {
     public static void printDifference(BigDecimal subtrahend, BigDecimal minuend) {   
         BigDecimal difference = subtrahend.subtract(minuend);
         System.out.println(subtrahend.stripTrailingZeros().toPlainString() + " - " + minuend.stripTrailingZeros().toPlainString() + " = " + difference.stripTrailingZeros().toPlainString());
+    }
+    
+    public static void printProduct(BigDecimal factorOne, BigDecimal factorTwo) {
+        BigDecimal product = factorOne.multiply(factorTwo);
+        System.out.println(factorOne.stripTrailingZeros().toPlainString() + " * " + factorTwo.stripTrailingZeros().toPlainString() + " = " + product.stripTrailingZeros().toPlainString());
+    }
+    
+    public static void printQuotient(BigDecimal dividend, BigDecimal divisor) {
+        BigDecimal quotient = dividend.divide(divisor, RoundingMode.HALF_UP);
+        System.out.println(dividend.stripTrailingZeros().toPlainString() + " / " + divisor.stripTrailingZeros().toPlainString() + " = " + quotient.stripTrailingZeros().toPlainString());
     }
 }
